4abf1b0dae892c7ce272011598967125f9d79619
Fabioex <fabioex@gmail.com>
2026-03-25 00:35:52 +0000

    Tema 4 - Xadrez - Nível Novato: Torre, bispo e rainha usando while, do-while e for.

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
-int main(void){
+/*Desafio Peças de Xadrez 
+Nível Novato: Torre, bispo e rainha.
+Tema 4 - Movimentação de Peças de Xadrez
+Objetivo: No nível Novato, você começará implementando a lógica básica de movimentação das peças usando while, do-while e for.
+Aluno: Fábio Oliveira Nunes Matrícula: 202601102478*/ 
 
-    printf("Desafio Jogo de Xadrez\n");
 
+
+int main() {
+
+   // Mover a Torre 5 casas para a direita
+
+   printf("Movimentação da Torre:\n");
+
+   for (int direita = 0; direita < 5; direita++) {
+        printf("Direita\n");
+    }
+
+    printf("\n"); // Adiciona uma linha em branco para separar as movimentações
+
+    // Mover o Bispo 5 casas para a diagonal superior direita
+
+    printf("Movimentação do Bispo:\n");
+
+    int cimaDireita = 0; //cimaDireita = diagonal superior direita
+    while (cimaDireita < 5) {
+        printf("Cima Direita\n");
+        cimaDireita++;
+    }
+
+    printf("\n"); // Adiciona uma linha em branco para separar as movimentações
+
+    // Mover a Rainha 8 casas para baixo
+
+    printf("Movimentação da Rainha:\n");
+
+    int esquerda = 0;
+    do {
+        printf("Esquerda\n");
+        esquerda++;
+    } while (esquerda < 8);
+
+    return 0;
 }
